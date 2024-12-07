f6251bafcc71d2cea56f96eee0a1d6e6b8a120df
Mike <misha995@yandex.ru>
2024-12-07 14:09:42 +0300

    исправил по пожеланиям ревьюера

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 final class ImagesListCell: UITableViewCell {
+    
+    // MARK: - Static properties
+    
     static let reuseIdentifier = "ImagesListCell"
+    
+    // MARK: - @IBOutlet properties
+    
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var likeButton: UIButton!
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class ImagesListViewController: UIViewController {
+final class ImagesListViewController: UIViewController {
     
+    // MARK: - @IBOutlet properties
+    @IBOutlet private var tableView: UITableView!
+    
+    // MARK: - Private Properties
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
     
     private lazy var dateFormatter: DateFormatter = {
@@ -17,32 +21,32 @@ class ImagesListViewController: UIViewController {
         formatter.timeStyle = .none
         return formatter
     }()
-
-    @IBOutlet private var tableView: UITableView!
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        guard let image = UIImage(named: photosName[indexPath.row]) 
+    private let currentDate = Date()
+    
+    // MARK: - Private Methods
+    private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row])
         else {
             return
         }
         cell.cellImage.image = image
-        cell.dateLabel.text = dateFormatter.string(from: Date())
+        cell.dateLabel.text = dateFormatter.string(from: currentDate)
         let isLiked = indexPath.row % 2 == 0
         let likeImage = isLiked ? UIImage(named: "Active") : UIImage(named: "No Active")
         cell.likeButton.setImage(likeImage, for: .normal)
-        
     }
+    
+    // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        super.viewDidLoad()
-
-        tableView.rowHeight = 200
+        
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
-
-
+    
+    
 }
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { } // TODO: - Добавить логику при нажатии на ячейку
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
             return 0
@@ -62,15 +66,15 @@ extension ImagesListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) // 1
-            
-            guard let imageListCell = cell as? ImagesListCell else { // 2
-                return UITableViewCell()
-            }
-            
-        configCell(for: imageListCell, with: indexPath) // 3
-            return imageListCell // 4
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) // 1
+        
+        guard let imageListCell = cell as? ImagesListCell else { // 2
+            return UITableViewCell()
         }
+        
+        configCell(for: imageListCell, with: indexPath) // 3
+        return imageListCell // 4
+    }
     
     
 }
