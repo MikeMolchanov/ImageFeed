5c1dbc3174519bc377f724acd4b6dc6a470b178b
Mike <misha995@yandex.ru>
2025-07-25 18:56:16 +0300

    коммит перед тестом

--- ImageFeed/SingleImageView/ImagesListService.swift
+++ ImageFeed/SingleImageView/ImagesListService.swift
@@ -57,27 +57,96 @@ final class ImagesListService {
     private(set) var photos: [Photo] = []
     
     private var lastLoadedPage: Int?
+    private var currentTask: URLSessionTask?
+    private var nextPage = 1
+    
+    
+    
+    
     
     func fetchPhotosNextPage() {
+        guard currentTask == nil else {
+            print(" Загрузка уже идет, пропускаем новый запрос " )
+            return
+        }
+        let pageToLoad = nextPage
+        let perPage = 10
+        guard let request = createRequest(page: pageToLoad, perPage: perPage) else {
+            print ("Ошибка: неверный запрос")
+            return
+        }
         
+        var currentTask = URLSession.shared.dataTask(with: request) {[weak self] data, _, error in
+            guard let self = self else {return}
+            defer{self.currentTask = nil} // Сброс флага при завершении
+            
+            if let error = error {
+                print("Ошибка сети: \(error.localizedDescription)")
+                return
+            }
+            guard let data = data else {
+                print("Ошибка: нет данных")
+                return}
+            
+            do {
+                let newPhotos = try self.parsePhotos(from: data) // Парсим JSON
+                DispatchQueue.main.async {
+                    self.nextPage += 1
+                    self.photos.append(contentsOf: newPhotos)
+                    NotificationCenter.default.post(name: ImagesListService.didChangeNotification,
+                                                    object: self)
+                }
+            } catch {
+                print("Ошибка парсинга: \(error)")
+            }
+        }
+        currentTask.resume()
+    }
+    
+    private func createRequest(page: Int, perPage: Int ) -> URLRequest? {
+        let baseURL = "https://api.unsplash.com/photos"
+        var components = URLComponents(string: baseURL)
+        components?.queryItems = [
+            URLQueryItem(name: "page", value: String(page)),
+            URLQueryItem(name: "per_page", value: String(perPage))]
+        
+        guard let url = components?.url else {
+            print("Ошибка: неверный URL")
+            return nil
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("695743 KLjJjKd0HAHZefLoKnLVZ4ZfoJSiksS-riusDQ7l-R8", forHTTPHeaderField: "Authorization") //найти свой ключ аккаунта Unsplash
+        return request
+    }
+    private func parsePhotos(from data: Data) throws -> [Photo] {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let photoResults = try decoder.decode([PhotoResult].self, from: data)
+        
+        // Конвертируем [PhotoResult] в [Photo]
+        return photoResults.map { Photo.from(photoResult: $0) }
     }
 }
 
-//extension Photo {
-//    static func from(photoResult: PhotoResult) -> Photo {
-//        // 1. Преобразуем дату из String в Date
-//        let dateFormatter = ISO8601DateFormatter()
-//        
-//        // 2. Создаём CGSize из width/height
-//        let size = CGSize(width: photoResult.width, height: photoResult.height)
-//        
-//        let createdAtDate = photoResult.createdAt.flatMap { dateFormatter.date(from: $0)}
-//        return Photo(id: photoResult.id,
-//                     size: size,
-//                     createdAt: createdAtDate,
-//                     welcomeDescription:photoResult.description,
-//                     thumbImageURL: photoResult.urls.thumb,
-//                     largeImageURL: photoResult.urls.full,
-//                     isLiked: photoResult.likedByUser)
-//    }
-//}
+extension Photo {
+    static func from(photoResult: PhotoResult) -> Photo {
+        // 1. Преобразуем дату из String в Date
+        let dateFormatter = ISO8601DateFormatter()
+        
+        // 2. Создаём CGSize из width/height
+        let size = CGSize(width: photoResult.width, height: photoResult.height)
+        
+        
+        let createdAtDate = dateFormatter.date(from: photoResult.createdAt)
+            
+        return Photo(id: photoResult.id,
+                     size: size,
+                     createdAt: createdAtDate,
+                     welcomeDescription:photoResult.description,
+                     thumbImageURL: photoResult.urls.thumb,
+                     largeImageURL: photoResult.urls.full,
+                     isLiked: photoResult.likedByUser)
+    }
+}
